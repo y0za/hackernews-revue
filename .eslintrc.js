52989eb5a5572cb5a6f38b01be2b8cc8467db2b7
yoza <yoza.is12s@gmail.com>
2016-03-05 19:15:46 +0900

    Modify eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,15 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': [2, {'anonymous': 'always', 'named': 'never'}],
+    'no-multi-spaces': 0,
+    'key-spacing': 0
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jasmine: true
   }
 }
